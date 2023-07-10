7a824963a49fcd70c6cfb7b0a2b9112b01473975
tourniquet <admyn3d@gmail.com>
2023-07-10 22:58:54 +0100

    refactored Breadcrumbs component

---
+++ src/app/item/components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const RootDiv = styled.div`
+  color: #E26433;
+  font-size: small;
+  font-weight: 600;
+  line-height: 1.65em;
+
+  a:link, a:visited, a:active, a:hover {
+    color: #E26433;
+    text-decoration: none;
+  }
+
+  .slash {
+    color: #9D9D9D;
+    padding: 0 12px;
+  }
+
+  .breadcrumb-ad-title {
+    color: #000;
+  }
+
+  @media (min-width: 1200px) {
+    margin-bottom: 33px;
+  }
+`
+
+interface BreadcrumbsParams {
+  category: string
+  subcategory: number
+  title: string
+};
+
+export default function Breadcrumbs ({ category, subcategory, title }: BreadcrumbsParams): JSX.Element {
+  return (
+    <RootDiv>
+      <span><a href='/home/page/1'>Home </a></span>
+      <span className='slash'>/ </span>
+      <span><a href={`/category/${category}/1`}>{category} </a></span>
+      <span className='slash'>/ </span>
+      <span><a href={`/${category}/${subcategory}/1`}>{subcategory}</a></span>
+      <span className='slash'>/ </span>
+      <span className='breadcrumb-ad-title'>{title}</span>
+    </RootDiv>
+  )
+}
